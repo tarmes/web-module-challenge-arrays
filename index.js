1f7648f247c22b4a15c08546f5675ddb0793af3e
tarmes <traarmes@gmail.com>
2020-07-15 16:50:26 -0400

    Task 6

--- index.js
+++ index.js
@@ -64,9 +64,10 @@ For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainb
 
 function addFlavor(arr){
   arr.unshift('Rainbow Sherbert');
+  console.log(originalFlavors);
 }
 
-console.log(addFlavor(originalFlavors));
+addFlavor(originalFlavors);
 
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array.
@@ -113,11 +114,16 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
-
-    /*code here*/
-
+function removeFlavorByName(arr, flavor){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === 'Vanilla'){
+      arr.splice(i,1);
+    }
+  }
+  return arr;
 }
+
+console.log(removeFlavorByName(originalFlavors, 'Vanilla'));
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array.
@@ -128,12 +134,12 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
-    /*code here*/
-
+function copy(newArr, oldArr){
+  var newArr = [...oldArr];
+  return newArr;
 }
 
+console.log(copy(newFlavors, originalFlavors));
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept:
